7e75b87422e05bebff4373ee1b86988d11042ee5
cuiwu <2946803165@qq.com>
2022-06-28 00:19:36 +0800

    排序算法总结

--- src/views/algorithm/List/Index.jsx
+++ src/views/algorithm/List/Index.jsx
@@ -4,7 +4,12 @@ export default function Index() {
   return (
     <div>
       <div className='content-item'>
-        <h3>1,链表排序</h3>
+        <h3>1,重排链表（注：不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换）</h3>
+        <pre>{`
+          function reset(head) {
+            console.log('重排链表')
+          }
+        `}</pre>
       </div>
     </div>
   )
--- src/views/algorithm/Sort/Index.jsx
+++ src/views/algorithm/Sort/Index.jsx
@@ -18,13 +18,11 @@ export default function Index() {
         <pre>{`
           function selectSort(arr) {
             for (let i = 0; i < arr.length; i++) {
-              let index = i
-              for (let j = i+1; j < arr.length; j++) {
-                if (arr[index]>arr[j]) index = j
+              for (let j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) [arr[i], arr[j]] = [arr[j], arr[i]];
               }
-              [arr[i], arr[index]] = [arr[index], arr[i]]
             }
-            return arr
+            return arr;
           }
         `}
         </pre>
@@ -120,6 +118,147 @@ export default function Index() {
           console.log(arr1) // [1,2,3,4,5,6,7]
         `}
         </pre>
+      </div>
+      <div className='content-item'>
+        <h3>6,希尔排序（个人理解：升级版的插入排序）</h3>
+        <p>有间隔的插入排序，让相同间隔的元素进行插入排序，依次递归，知道间隔为1，即最后一个插入排序，整个数组排序成功</p>
+        <pre>{`
+          采用对半的方式设置每次的间隔
+          function shellSort(arr) {
+            for (let gap = parseInt(arr.length / 2); gap >= 1;gap = parseInt(gap / 2)) {
+              for (let i = gap; i < arr.length; i++) {
+                for (j = i; j > gap - 1; j -= gap) {
+                  if (arr[j] < arr[j - gap]) {
+                    [arr[j], arr[j - gap]] = [arr[j - gap], arr[j]];
+                  }
+                }
+              }
+            }
+            return arr;
+          }
+          利用Knuth序列（效率比对半的高）
+          function shellSort(arr) {
+            let h = 1
+            while(h<=arr.length/3) {
+              h=h*3+1
+            }
+            for (let gap = h; gap > 0; gap = (gap-1)/3) {
+              for (let i = gap; i < arr.length; i++) {
+                for (j = i; j > gap - 1; j -= gap) {
+                  if (arr[j] < arr[j - gap]) {
+                    [arr[j], arr[j - gap]] = [arr[j - gap], arr[j]];
+                  }
+                }
+              }
+            }
+            return arr;
+          }
+        `}</pre>
+      </div>
+      <div className='content-item'>
+        <h3>6,计数排序（基于桶排序思想，不通过比较排序）</h3>
+        <p>
+          找出待排序的数组中最大和最小的元素，然后建立一个数组C用于统计待排数组中最小元素到最大元素区间中每个元素出现次数，
+          统计数组中每个值为i的元素出现的次数，存入数组C的第i项，
+          对所有的计数累加（从C中的第一个元素开始，每一项和前一项相加），
+          反向填充目标数组：将每个元素i放在新数组的第C(i)项，每放一个元素就将C(i)减去1
+        </p>
+        <pre>{`
+          function jishuSort(arr) {
+            let res = [];
+            let result = [0, 0, 0, 0, 0];
+            for (let i = 0; i < arr.length; i++) {
+              result[arr[i]]++;
+            }
+            for (let i = Math.min(...arr), j = 0; i < result.length; i++) {
+              while (result[i]-- > 0) {
+                res[j++] = i;
+              }
+            }
+            return res;
+          }
+        `}</pre>
+      </div>
+      <div className='content-item'>
+        <h3>7,基数排序（基于桶排序思想，不通过比较排序）</h3>
+        <p>先将数组按照个位数进行排序，然后十位，以此类推，总的排序次数按照数组的最长位数数据，其他不够的补0</p>
+        <pre>{`
+          function radixSort(array) {
+            let length = array.length;
+            // 如果不是数组或者数组长度小于等于1，直接返回，不需要排序
+            if (!Array.isArray(array) || length <= 1) return;
+            let bucket = [],
+              max = array[0],
+              loop;
+            // 确定排序数组中的最大值
+            for (let i = 1; i < length; i++) {
+              if (array[i] > max) {
+                max = array[i];
+              }
+            }
+            // 确定最大值的位数
+            loop = (max + '').length;
+            // 初始化桶
+            for (let i = 0; i < 10; i++) {
+              bucket[i] = [];
+            }
+            for (let i = 0; i < loop; i++) {
+              for (let j = 0; j < length; j++) {
+                let str = array[j] + '';
+                if (str.length >= i + 1) {
+                  let k = parseInt(str[str.length - 1 - i]); // 获取当前位的值，作为插入的索引
+                  bucket[k].push(array[j]);
+                } else {
+                  // 处理位数不够的情况，高位默认为 0
+                  bucket[0].push(array[j]);
+                }
+              }
+              array.splice(0, length); // 清空旧的数组
+              // 使用桶重新初始化数组
+              for (let i = 0; i < 10; i++) {
+                let t = bucket[i].length;
+                for (let j = 0; j < t; j++) {
+                  array.push(bucket[i][j]);
+                }
+                bucket[i] = [];
+              }
+            }
+            return array;
+          }
+        `}</pre>
+      </div>
+      <div className='content-item'>
+        <h3>8,桶排序</h3>
+        <p>将数组分到有限数量的桶子里。每个桶子再个别排序（有可能再使用别的排序算法或是以递归方式继续使用桶排序进行排序）</p>
+        <pre>{`
+          function bucketSort(arr, bucketSize) {
+            if (arr.length === 0) {
+              return arr;
+            }
+            let maxValue = Math.max(...arr);
+            let minValue = Math.min(...arr);
+            //桶的初始化
+            var DEFAULT_BUCKET_SIZE = 5;            // 设置桶的默认数量为5
+            bucketSize = bucketSize || DEFAULT_BUCKET_SIZE;
+            var bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;  
+            var buckets = new Array(bucketCount);
+            for (i = 0; i < buckets.length; i++) {
+              buckets[i] = [];
+            }
+            //利用映射函数将数据分配到各个桶中
+            for (i = 0; i < arr.length; i++) {
+              buckets[Math.floor((arr[i] - minValue) / bucketSize)].push(arr[i]);
+            }
+            arr.length = 0;
+            for (i = 0; i < buckets.length; i++) {
+              insertionSort(buckets[i]); // 对每个桶进行排序，这里使用了插入排序，这里也可以直接用js的数组sort方法，很直接
+              for (var j = 0; j < buckets[i].length; j++) {
+                arr.push(buckets[i][j]);                      
+              }
+            }
+            return arr;
+          }
+        `}</pre>
       </div>
     </div>
   )
